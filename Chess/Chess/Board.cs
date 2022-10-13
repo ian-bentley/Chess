ec212368932a69ce3e85a2ce2f6b9d769cab557f
Ian Bentley <ibentley981203@gmail.com>
2022-10-13 14:16:34 -0700

    Created Board class to handle the board
    - Cleaned up commented code
    - Added a Board class
    - Added a Fill method to Board class to fill the board with tiles
    - Added a Draw method to Board class to draw the board

---
+++ Chess/Chess/Board.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    class Board
+    {
+        string tileEmpty = "[ ]";
+        const int length = 8;
+        const int width = 8;
+        string[,] tiles = new string[length, width];
+
+        public Board()
+        {
+            Fill();
+        }
+        void Fill()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    tiles[i, j] = tileEmpty;
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(tiles[i, j]);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
--- Chess/Chess/Program.cs
+++ Chess/Chess/Program.cs
@@ -8,21 +8,8 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            /*Console.WriteLine("Hello World!");
-            string input = Console.ReadLine();
-            Console.WriteLine(input);*/
-
-            string emptyTile = "[ ]";
-            int boardLength = 8;
-            int boardWidth = 8;
-            for (int i = 0; i < boardLength; i ++)
-            {
-                for (int j = 0; j < boardWidth; j++)
-                {
-                    Console.Write(emptyTile);
-                }
-                Console.Write("\n");
-            }
+            Board board = new Board();
+            board.Draw();
         }
     }
 }
